22cd141b013a702c87be48977f7bb7d30da54289
ingihong06 <87604152+ingihong06@users.noreply.github.com>
2022-07-13 19:59:10 +0900

    Monster너ㅏㅁㄴㅁ어랴ㅐㅣㅁㄴ오래ㅑㅔㅁㄴㅇ로ㅓㅏㅁㄴㅇ로ㅝㅏㄴ오ㅓㅏㅣ;

--- Assets/99_Boss/01_scripts/PatternType.cs
+++ Assets/99_Boss/01_scripts/PatternType.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class PatternType : MonoBehaviour
 {
-    
+    private Movement movement;
+    private float distance;
+    private Transform player;
+    private Animator animator;
+
+    private bool isOnAttack = false;
+
     private void Start()
     {
-
+        movement = GetComponent<Movement>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
     }
     private void Awake()
     {
-        Pattern();
+        //Pattern();
+        CloseAttack();
+    }
+    private void Update()
+    {
+        distance = Vector3.Distance(player.position, transform.position);
     }
     private IEnumerator Pattern()
     {
@@ -19,6 +32,14 @@ public class PatternType : MonoBehaviour
     }
     private IEnumerator CloseAttack()
     {
+        isOnAttack = true;
+        while (distance > 3)
+        {
+            movement.ChasePlayer();
+        }
+        animator.SetTrigger("A");
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("f");
         yield return null;
     }
     private IEnumerator Cast()
