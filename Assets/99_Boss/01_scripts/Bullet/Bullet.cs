3668da13d6e85f6bf229c7257f115e79c0c2f09a
ingihong06 <87604152+ingihong06@users.noreply.github.com>
2022-07-14 12:41:24 +0900

    Boss1Pattern

--- Assets/99_Boss/01_scripts/Bullet/Bullet.cs
+++ Assets/99_Boss/01_scripts/Bullet/Bullet.cs
@@ -3,75 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class Bullet : MonoBehaviour
+public class Bullet : PoolAble
 {
     [SerializeField]
     private GameObject explosion;
     private Vector3 direction;
 
-    private float _angle;
+    private Vector3 diff;
 
-    //addforce
-    [SerializeField]
-    private float speed = 5;
-
+    private float rot_z;
+        
     private Transform player;
 
     private void Start()
     {
+        StartCoroutine(Destroid());
+        Instantiate(explosion, transform.position, Quaternion.identity);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 
-        Vector3 direction = (transform.position - player.transform.position).normalized * -100;
+        direction = (transform.position - player.transform.position).normalized * -100;
         transform.DOMove(direction, 30f);
-        //transform.DOLookAt(direction, 1f);
 
-        Vector3 diff = player.transform.position - transform.position;
+        diff = player.transform.position - transform.position;
         diff.Normalize();
 
-        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
-
-
-    private void Update()
-    {
-
-        //transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime *15);
-        //transform.position += direction * speed * Time.deltaTime;
-    }
-
-
-
-
-
-
-
     private void OnDestroy()
     {
-        //Instantiate(explosion);
+        Debug.Log("Dead");
+        Instantiate(explosion, transform.position, Quaternion.identity);
     }
-    private void OnBecameInvisible()
+    private IEnumerator Destroid()
     {
-        //StartCoroutine(OutOfCamera());
-    }
-    private IEnumerator OutOfCamera()
-    {
-        yield return new WaitForSeconds(2);
+        Debug.Log("sex");
+        yield return new WaitForSeconds(10);
         Destroy(gameObject);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" || collision.tag == "Platform")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
-
-
-/*
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-
-        Vector3 direction = (transform.position - player.transform.position).normalized * -100;
-        transform.DOMove(direction, 30f);
-        //transform.DOLookAt(direction, 1f);
-
-        Vector3 diff = player.transform.position - transform.position;
-        diff.Normalize();
-
-        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
-*/
--- Assets/99_Boss/01_scripts/PatternType.cs
+++ Assets/99_Boss/01_scripts/PatternType.cs
@@ -9,8 +9,20 @@ public class PatternType : MonoBehaviour
     private Transform player;
     private Animator animator;
 
-    private bool isOnAttack = false;
+    [SerializeField]
+    private GameObject bulletPrefabs;
 
+    //private bool isOnAttack = false;
+
+    [SerializeField]
+    private float groundPositionY; //지면과 Boss의 Collider가 맞닿는 지점
+
+
+    //보스 궁극기 사용시 이동할 지점
+    [SerializeField]
+    private float middleOfMapX; //보스맵의 중앙 X
+    [SerializeField]
+    private float highPointY; //맵의 위쪽
     private void Awake()
     {
         //Pattern();
@@ -23,31 +35,58 @@ public class PatternType : MonoBehaviour
     private void Start()
     {
         //StartCoroutine(CloseAttack());
+        StartCoroutine(OpeningPattern());
     }
     private void Update()
     {
-        distance = Vector3.Distance(player.position, transform.position);
-        if (distance > 3f)
-        {
-            Debug.Log("aaaaaaaaaaaaaaaaa");
-            movement.ChasePlayer();   
-        } else if (distance < 3)
-        {
-            StartCoroutine(CloseAttack());
-        }
+        //distance = Vector3.Distance(player.position, transform.position);
+        //if (distance > 3f)
+        //{
+        //    Debug.Log("aaaaaaaaaaaaaaaaa");
+        //    movement.ChasePlayer();   
+        //} else if (distance < 3)
+        //{
+        //    StartCoroutine(CloseAttack());
+        //}
     }
     private IEnumerator Pattern()
     {
         yield return null;
     }
+    private IEnumerator OpeningPattern()
+    {
+        animator.SetTrigger("T");//Attack
+        StartCoroutine(castBulletAtSide());
+        yield return new WaitForSeconds(0.5f);
+        animator.SetTrigger("A");
+        transform.position = new Vector3(player.position.x + 3, groundPositionY, 0);
+        yield return new WaitForSeconds(1f);
+        animator.SetTrigger("I");
+    }
     private IEnumerator CloseAttack()
     {
         Debug.Log("a");
-        isOnAttack = true;
+        //isOnAttack = true;
         animator.SetTrigger("A");
         yield return new WaitForSeconds(0.5f);
         Debug.Log("f");
-        isOnAttack = false;
+        //isOnAttack = false;
+        yield return null;
+    }
+
+    private IEnumerator castBulletThreeTime()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(bulletPrefabs, new Vector3(transform.position.x, transform.position.y + 2, 0), Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Instantiate(bulletPrefabs, new Vector3(transform.position.x, transform.position.y + 2, 0), Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Instantiate(bulletPrefabs, new Vector3(transform.position.x, transform.position.y + 2, 0), Quaternion.identity);
+    }
+    private IEnumerator castBulletAtSide()
+    {
+        Instantiate(bulletPrefabs, new Vector3(transform.position.x - 3, transform.position.y + 2, 0), Quaternion.identity);
+        Instantiate(bulletPrefabs, new Vector3(transform.position.x + 3, transform.position.y + 2, 0), Quaternion.identity);
         yield return null;
     }
     private IEnumerator Cast()
